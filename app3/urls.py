a000553bf104f1006ced555bdc739b8f61ad7bfd
anirudhmore1 <anirudh1@vt.edu>
2024-02-06 15:12:27 -0500

    demo - lawnmower for each quadrant (have to correct starting postion) and demoworking - object detected

--- app3/urls.py
+++ app3/urls.py
@@ -14,10 +14,10 @@ router.register(r'gpshistoricaldata', views.GPShistoricalDataViewSet,basename="g
 
 urlpatterns = [
     #path('', TaskGenerationView.as_view(),name='sarwebinit'),
-    path('',TemplateView.as_view(template_name="demo_landingpage.html")),
-    path('sarwebinit', TaskGenerationView.as_view(),name='sarwebinit'),
+    path('',TemplateView.as_view(template_name="demo_landingpage.html")), #landing page
+    path('leeshwebinit', TaskGenerationView.as_view(),name='leeshwebinit'),
 
-    path('demoinit',TemplateView.as_view(template_name="demo.html"), name="demoinit"),
+    path('demoinit',TemplateView.as_view(template_name="demo.html"), name="demoinit"), #TCIN simulation
 
     path('sarweb3D', TaskGenerationView3D.as_view(),name='sarweb3D'),
     path('sarweb3DDemo', TaskGenerationView3DDemo.as_view(),name='sarweb3DDemo'),
